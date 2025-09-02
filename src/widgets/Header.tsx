4837286dd8f2b16d408fe89742c75983d99a31d8
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-09-02 14:59:14 +0400

    style: add responsive styles for header

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,26 +1,27 @@
+'use client';
+import Link from 'next/link';
 import React from 'react';
 
 function Header() {
   return (
-    <header className="text-neutral-content sticky top-0 z-2 flex items-center justify-between gap-3 bg-sky-950 p-3">
-      <div className="w-50 text-xl">REST Client</div>
-      <div className="buttons-block flex gap-2">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn rounded-sm">
-            Language
-          </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
-          >
-            <li>
-              <a>EN</a>
-            </li>
-            <li>
-              <a>RU</a>
-            </li>
-          </ul>
-        </div>
+    <header className="text-neutral-content sticky top-0 z-2 flex flex-wrap items-center justify-center gap-3 bg-sky-950 p-3 md:flex-nowrap md:justify-between">
+      <div className="w-50 text-center text-xl">
+        <Link
+          href="/"
+          className="btn btn-ghost rounded-sm text-xl normal-case hover:bg-sky-900"
+        >
+          REST Client
+        </Link>
+      </div>
+      <div className="buttons-block flex w-full flex-wrap justify-center gap-5 md:flex-nowrap md:justify-end">
+        <label className="swap">
+          <input
+            type="checkbox"
+            onChange={(event) => console.log(event.target.value)}
+          />
+          <div className="swap-on">EN</div>
+          <div className="swap-off">RU</div>
+        </label>
         <button className="btn btn-soft btn-primary rounded-sm">Sign In</button>
         <button className="btn btn-primary rounded-sm">Sign Up</button>
       </div>

f5c64e2249713873e73f7a3ffa75f5e52716f268
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-09-02 12:21:46 +0400

    fix: replace dropdowm element

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="bg-base-700 text-neutral-content flex items-center justify-between gap-3 p-3">
+    <header className="text-neutral-content sticky top-0 z-2 flex items-center justify-between gap-3 bg-sky-950 p-3">
       <div className="w-50 text-xl">REST Client</div>
       <div className="buttons-block flex gap-2">
-        <details className="dropdown">
-          <summary className="btn rounded-sm">Language</summary>
-          <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn rounded-sm">
+            Language
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
             <li>
               <a>EN</a>
             </li>
@@ -15,7 +20,7 @@ function Header() {
               <a>RU</a>
             </li>
           </ul>
-        </details>
+        </div>
         <button className="btn btn-soft btn-primary rounded-sm">Sign In</button>
         <button className="btn btn-primary rounded-sm">Sign Up</button>
       </div>

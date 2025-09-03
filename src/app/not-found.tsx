a742f8c5a36419fc1017252016b93e1582018043
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-09-03 14:25:17 +0400

    feat: implement not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <div className="page-404 bg-base-300 flex h-screen flex-col items-center justify-start gap-10 pt-10 pb-5">
+      <Image
+        src="/404.png"
+        width={300}
+        height={300}
+        alt="page-not-found image"
+        className="image-404"
+        priority
+      ></Image>
+      <h3 className="text-center text-xl">
+        Sorry, page not found. Probably wrong URL. Probably page stolen by
+        aliens.
+      </h3>
+      <Link href="/" className="btn btn-secondary btn-soft rounded-sm text-lg">
+        Back to home
+      </Link>
+    </div>
+  );
+}
